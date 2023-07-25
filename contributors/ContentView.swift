797f64aba6323d6dc0a0f289fb6f7768467cea5f
Eduardo Hoyos <edu.hoyos92@gmail.com>
2023-07-25 11:28:23 -0500

    First commit ever

---
+++ contributors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  contributors
+//
+//  Created by Eduardo Hoyos Loli on 25/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ contributors/contributorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  contributorsApp.swift
+//  contributors
+//
+//  Created by Eduardo Hoyos Loli on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct contributorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
